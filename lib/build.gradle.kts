fadb07fe16f28a1132f07151a0db97f68f8019cb
Heizi <xm.zhongjiaxin@gmail.com>
2023-03-26 01:12:48 +0800

    create licenses and pom

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -24,6 +24,47 @@ allprojects {
                 name = "test"
                 url = uri("file://${rootProject.projectDir}/build/maven-repo/")
             }
+            maven {
+                name = "github"
+                url = uri("https://maven.pkg.github.com/ElisaMin/MonetCompose")
+                credentials {
+                    username = System.getenv("GITHUB_ACTOR")
+                    password = System.getenv("GITHUB_TOKEN")
+                }
+            }
+        }
+
+        publications {
+            val pom = { it:MavenPublication -> with(it) {
+                pom {
+                    name.set("Kdrag0nMonetCompose")
+                    description.set("A Compose [windows only now] library for generating color palettes based on Kdrag0n's Monet Engine")
+                    url.set("https://github.com/ElisaMin/MonetCompose")
+                    licenses {
+                        license {
+                            name.set("MIT License")
+                            url.set("https://github.com/ElisaMin/MonetCompose/blob/main/LICENSE")
+                        }
+                        license {
+                            name.set("MIT License - Heizi")
+                            url.set("https://github.com/ElisaMin/MonetCompose/blob/main/lib/LICENSE")
+                        }
+                    }
+                    developers {
+                        developer {
+                            id.set("ElisaMin")
+                            name.set("ElisaMin")
+                            email.set("Heizi@lge.fun")
+                        }
+                    }
+                    scm {
+                        connection.set("scm:git:git://github.com/ElisaMin/MonetCompose.git")
+                        developerConnection.set("scm:git:ssh://github.com/ElisaMin/MonetCompose.git")
+                        url.set("https://github.com/ElisaMin/MonetCompose")
+                    }
+                }
+            } }
+            filterIsInstance<MavenPublication>().forEach(pom)
         }
     }
 }
